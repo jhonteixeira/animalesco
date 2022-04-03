24cff29818fc92120fbb8ca7efea25944631d57c
jhonteixeira <jhonatan93_@hotmail.com>
2022-04-03 18:55:05 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import Jogo from "./Jogo.js"
 import animais from './Animais.js'
+import readline from 'readline'
 
-let jogo = new Jogo('Jhonatan', 'Teixeira', animais)
-jogo.jogar()
+const nomes = animais.map(animal => animal.nome)
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('Animalesco> ');
+rl.prompt();
+
+rl.on('line', function (line) {
+    const trimmed = line.trim()
+
+    const animal = animais.find(animal => animal.nome.toLowerCase() === trimmed)
+    if (animal) {
+        console.log(animal)
+    } else {
+        console.log('Tendi não... Escutei vc dizendo `' + trimmed + '`.')
+    }
+
+    rl.prompt();
+}).on('close', function () {
+    console.log('Ah, que pena q vc tá indo embora... Obrigado por jogar Animalesco!');
+    process.exit(0);
+});
